8a9da208132bb001da7b4bb6afefdc805c748137
yangzhifei <1406142763@qq.com>
2016-03-14 20:57:58 +0800

    界面的初次修改-2016-03-14
    初次修改界面
    1.移除SHOW LOG的menuIetm
    2.更改蓝牙列表为独立的界面，并不是Dialog。

--- BluetoothChat/app/src/main/java/com/ckt/yzf/bluetoothchat/bluetoothchat/MainActivity.java
+++ BluetoothChat/app/src/main/java/com/ckt/yzf/bluetoothchat/bluetoothchat/MainActivity.java
@@ -64,27 +64,12 @@ public class MainActivity extends SampleActivityBase {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
-        logToggle.setVisible(findViewById(R.id.sample_output) instanceof ViewAnimator);
-        logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);
 
         return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.menu_toggle_log:
-                mLogShown = !mLogShown;
-                ViewAnimator output = (ViewAnimator) findViewById(R.id.sample_output);
-                if (mLogShown) {
-                    output.setDisplayedChild(1);
-                } else {
-                    output.setDisplayedChild(0);
-                }
-                supportInvalidateOptionsMenu();
-                return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -99,12 +84,5 @@ public class MainActivity extends SampleActivityBase {
         // Filter strips out everything except the message text.
         MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
         logWrapper.setNext(msgFilter);
-
-        // On screen logging via a fragment with a TextView.
-        LogFragment logFragment = (LogFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.log_fragment);
-        msgFilter.setNext(logFragment.getLogView());
-
-        Log.i(TAG, "Ready");
     }
 }
